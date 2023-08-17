370eeddfc3e90cdd850961145d4f128818810b5c
dasbiplob <biplob90.d@git.com>
2023-08-18 00:17:11 +0300

    Dveloped the FormGroup and FormControl for Save Video

--- frontend/youtube-clone-ui/src/app/app.module.ts
+++ frontend/youtube-clone-ui/src/app/app.module.ts
@@ -5,13 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UploadVideoComponent } from './upload-video/upload-video.component';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {NgxFileDropModule} from "ngx-file-drop";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import { HeaderComponent } from './header/header.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatOptionModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
 import { SaveVideoDetailsComponent } from './save-video-details/save-video-details.component';
 
 @NgModule({
@@ -30,7 +35,13 @@ import { SaveVideoDetailsComponent } from './save-video-details/save-video-detai
     NgxFileDropModule,
     MatButtonModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    FlexLayoutModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
